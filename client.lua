ba81ee5f64cebac3493b2b1851368b39ee87bda5
Fidibus08 <138357112+Fidibus08@users.noreply.github.com>
2026-01-01 12:19:22 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,83 @@
+local cruiseActive = false
+local cruiseSpeed = 0.0
+
+-- LEFT CTRL
+local CRUISE_KEY = 36
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+
+        local ped = PlayerPedId()
+
+        if IsPedInAnyVehicle(ped, false) then
+            local vehicle = GetVehiclePedIsIn(ped, false)
+
+            if IsControlJustPressed(0, CRUISE_KEY) then
+                if not cruiseActive then
+                    cruiseSpeed = GetEntitySpeed(vehicle)
+                    SetVehicleMaxSpeed(vehicle, cruiseSpeed)
+                    cruiseActive = true
+                else
+                    SetVehicleMaxSpeed(
+                        vehicle,
+                        GetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel")
+                    )
+                    cruiseActive = false
+                end
+            end
+        else
+            cruiseActive = false
+        end
+
+        DrawSpeedo()
+    end
+end)
+
+function DrawSpeedo()
+    local ped = PlayerPedId()
+    if not IsPedInAnyVehicle(ped, false) then return end
+
+    local vehicle = GetVehiclePedIsIn(ped, false)
+    local speedMPH  = math.floor(GetEntitySpeed(vehicle) * 2.23694)
+    local cruiseMPH = math.floor(cruiseSpeed * 2.23694)
+
+    -- Position links neben der Minimap
+    local x = 0.255
+    local y = 0.82
+    local w = 0.16
+    local h = 0.055
+
+    -- Hintergrund (flach & modern)
+    DrawRect(x, y, w, h, 10, 10, 10, 160)
+
+    -- Akzentlinie
+    if cruiseActive then
+        DrawRect(x - w / 2 + 0.002, y, 0.004, h, 0, 170, 255, 255)
+    else
+        DrawRect(x - w / 2 + 0.002, y, 0.004, h, 180, 180, 180, 120)
+    end
+
+    -- SPEED
+    DrawText2D("SPEED", x - 0.07, y - 0.015, 0.32, 170)
+    DrawText2D(speedMPH .. " MPH", x - 0.07, y + 0.005, 0.45, 255)
+
+    -- CRUISE
+    if cruiseActive then
+        DrawText2D("CRUISE", x + 0.02, y - 0.015, 0.32, 170)
+        DrawText2D(cruiseMPH .. " MPH", x + 0.02, y + 0.005, 0.45, 0, 170, 255)
+    else
+        DrawText2D("CRUISE", x + 0.02, y - 0.015, 0.32, 170)
+        DrawText2D("OFF", x + 0.02, y + 0.005, 0.45, 200, 80, 80)
+    end
+end
+
+function DrawText2D(text, x, y, scale, r, g, b)
+    SetTextFont(4)
+    SetTextScale(scale, scale)
+    SetTextColour(r or 255, g or 255, b or 255, 255)
+    SetTextOutline()
+    BeginTextCommandDisplayText("STRING")
+    AddTextComponentSubstringPlayerName(text)
+    EndTextCommandDisplayText(x, y)
+end
